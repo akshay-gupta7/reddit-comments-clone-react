41a814f36d79da7112c0cafd30a38d15036d2350
Akshay Gupta <gravity.akshay@gmail.com>
2023-04-03 00:34:15 +0530

    Add Post.js and PostContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import { PostList } from "./components/PostLists";
 import { Routes, Route } from "react-router";
+import { Post } from "./components/Post";
+import { PostProvider } from "./context/PostContext";
 
 function App() {
   return (
     <div className="container">
       <Routes>
         <Route path="/" element={<PostList />} />
-        <Route path="/posts/:id" element={<h1>Post</h1>} />
+        <Route
+          path="/posts/:id"
+          element={
+            <PostProvider>
+              <Post />{" "}
+            </PostProvider>
+          }
+        />
       </Routes>
     </div>
   );
