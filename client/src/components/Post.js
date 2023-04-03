a8c0ab81fb9e8630a35c25fffaa178b718dcb43c
Akshay Gupta <gravity.akshay@gmail.com>
2023-04-04 00:18:39 +0530

    Add comments service and add post comment on client side

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,16 +1,35 @@
 import { usePost } from "../context/PostContext";
 import { CommentForm } from "./CommentForm";
 import { CommentList } from "./CommentList";
+import { useAsync } from "../hooks/useAsync";
+import { useAsyncFn } from "../hooks/useAsync";
+import { createComment } from "../services/comments";
 
 export function Post() {
   const { post, rootComments } = usePost();
+  const {
+    loading,
+    error,
+    execute: createCommentFn,
+  } = useAsyncFn(createComment);
+
+  function onCommentCreate(message) {
+    return createCommentFn({ postId: post.id, message }).then((comment) => {
+      console.log(comment);
+    });
+  }
+
   return (
     <>
       <h1>{post.title}</h1>
       <article>{post.body}</article>
       <h3 className="comments-title">Comments</h3>
       <section>
-        <CommentForm />
+        <CommentForm
+          loading={loading}
+          error={error}
+          onSubmit={onCommentCreate}
+        />
         {rootComments != null && rootComments.length > 0 && (
           <div className="mt-4">
             <CommentList comments={rootComments} />
