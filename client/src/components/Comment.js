3eb3f098ca2ee42875979329c271db3f87a949bd
Akshay Gupta <gravity.akshay@gmail.com>
2023-04-04 01:06:25 +0530

    Add comment replying react logic in Comment component

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -3,6 +3,9 @@ import { FaEdit, FaHeart, FaReply, FaTrash } from "react-icons/fa";
 import { usePost } from "../context/PostContext";
 import { CommentList } from "./CommentList";
 import { useState } from "react";
+import { useAsync, useAsyncFn } from "../hooks/useAsync";
+import { createComment } from "../services/comments";
+import { CommentForm } from "./CommentForm";
 
 const dateFormatter = new Intl.DateTimeFormat(undefined, {
   dateStyle: "medium",
@@ -10,9 +13,21 @@ const dateFormatter = new Intl.DateTimeFormat(undefined, {
 });
 
 export function Comment({ id, message, user, createdAt }) {
-  const { getReplies } = usePost();
+  const { post, getReplies, createLocalComment } = usePost();
   const childComments = getReplies(id);
   const [areChildrenHidden, setAreChildrenHidden] = useState(false);
+  const [isReplying, setIsReplying] = useState(false);
+  const createCommentFn = useAsyncFn(createComment);
+
+  function onCommentReply(message) {
+    return createCommentFn
+      .execute({ postId: post.id, message, parentId: id })
+      .then((comment) => {
+        setIsReplying(false);
+        createLocalComment(comment);
+      });
+  }
+
   return (
     <>
       <div className="comment">
@@ -25,11 +40,26 @@ export function Comment({ id, message, user, createdAt }) {
         <div className="message">{message}</div>
         <div className="footer">
           <IconBtn Icon={FaHeart} aria-label="Like" />
-          <IconBtn Icon={FaReply} aria-label="Reply" />
+          <IconBtn
+            onClick={() => setIsReplying((prev) => !prev)}
+            isActive={isReplying}
+            Icon={FaReply}
+            aria-label={isReplying ? "Cancel Reply" : "Reply"}
+          />
           <IconBtn Icon={FaEdit} aria-label="Edit" />
           <IconBtn Icon={FaTrash} aria-label="Delete" color="danger" />
         </div>
       </div>
+      {isReplying && (
+        <div className="mt-1 ml-3">
+          <CommentForm
+            autoFocus
+            onSubmit={onCommentReply}
+            loading={createCommentFn.loading}
+            error={createCommentFn.error}
+          />
+        </div>
+      )}
       {childComments?.length > 0 && (
         <>
           <div
